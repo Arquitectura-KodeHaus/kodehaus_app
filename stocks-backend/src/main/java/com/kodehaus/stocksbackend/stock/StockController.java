046a9e42198aca74b8f27a0a065cb21348110eb9
Juan Diego Arias Duran <63438568+ElAreaAl2@users.noreply.github.com>
2025-10-10 12:32:35 -0500

    Update StockController.java

--- stocks-backend/src/main/java/com/kodehaus/stocksbackend/stock/StockController.java
+++ stocks-backend/src/main/java/com/kodehaus/stocksbackend/stock/StockController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.http.HttpStatus;
