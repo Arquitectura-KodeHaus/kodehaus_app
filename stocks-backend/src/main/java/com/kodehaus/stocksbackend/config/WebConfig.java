2449de66d91fc2b3f11b69541c26124da05ed1c6
Juan Diego Arias Duran <63438568+ElAreaAl2@users.noreply.github.com>
2025-10-11 18:25:41 -0500

    Update WebConfig.java

--- stocks-backend/src/main/java/com/kodehaus/stocksbackend/config/WebConfig.java
+++ stocks-backend/src/main/java/com/kodehaus/stocksbackend/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns(
                     "http://localhost:*",
                     "https://storage.googleapis.com",
+                    "https://stocks-frontend-bucket.storage.googleapis.com",
                     "https://*.storage.googleapis.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
