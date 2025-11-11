92c095093a3823e32425da15820a584967380c35
ElAreaAl2 <juandiegoariasd@hotmail.com>
2025-11-10 20:46:29 -0500

    Feat: update application properties for plazas service URL and set production API URL

--- stocks-frontend/src/environments/environment.prod.ts
+++ stocks-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '${API_URL}'
+  apiUrl: 'https://stocks-backend-service-616328447495.us-central1.run.app' // Solo tu backend en producción
 };
