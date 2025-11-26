91ae3687dff7082ed5bb53c331969a202bc6841a
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2025-11-26 16:03:18 -0500

    back gerente

--- stocks-backend/src/main/java/com/kodehaus/stocksbackend/controller/CuentaGerenteController.java
+++ stocks-backend/src/main/java/com/kodehaus/stocksbackend/controller/CuentaGerenteController.java
@@ -52,6 +52,7 @@ public class CuentaGerenteController {
         return ResponseEntity.ok(cuenta);
     }
 
+   
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody CuentaGerenteRequest datos) {
         // Validar que la contraseña tenga al menos 6 caracteres
@@ -63,34 +64,20 @@ public class CuentaGerenteController {
 
         CuentaGerenteDTO newCuenta = cuentaService.create(datos);
 
-        // Partir el nombre
-        String nombre = newCuenta.nombre();
-        String[] nameParts = nombre.split(" ");
-        String firstName = nameParts[0];
-
-        // Construir JSON EXACTO que el otro servicio espera
+        //Json enviado al otro servicio
         Map<String, Object> body = new HashMap<>();
-        body.put("externalId", String.valueOf(newCuenta.cedula()));   // lo ideal es usar la cédula como externalId
-        body.put("nombre", newCuenta.nombre());
+        body.put("username", newCuenta.correo());
         body.put("email", newCuenta.correo());
-        body.put("rol", "MANAGER");                                   // corresponde con req.getRol()
-        
-        // IMPORTANTE: plazaExternalId debe coincidir con lo que el otro servicio guarda en la BD
-        body.put("plazaExternalId", String.valueOf(newCuenta.plaza_id())); 
-        
-        body.put("phoneNumber", "+1-555-0005");
+        body.put("password", newCuenta.password());
+        body.put("firstName", newCuenta.nombre());
+        body.put("lastName", ".");
+        body.put("phoneNumber", "3008522478");
+        body.put("plazaId", newCuenta.plaza_id());
+        body.put("externalId", newCuenta.id());
 
-        System.out.println("JSON enviado al otro servicio: " + body);
+        System.out.println("Informacion enviada: " + body);
+        restTemplate.postForObject(gestionPlazasUrl + "/api/auth/external-register", body, Void.class);
 
-        // Llamada al servicio java-0
-        restTemplate.postForObject(
-            "https://backend-service-java-2-616328447495.us-central1.run.app/api/users/externo",
-            //"http://localhost:8070/api/users/externo",
-            body,
-            Void.class
-        );
-
-        return new ResponseEntity<>(newCuenta, HttpStatus.CREATED);
-    }
-
+        return new ResponseEntity<>(newCuenta, HttpStatus.CREATED); 
+    } 
 }
